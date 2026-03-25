4db1beca3f6d3bd9033c5557b7fe60a26caf7028
DhananjayaKularatne <djaya2100@gmail.com>
2026-03-25 11:03:42 +0530

    fixed import errors

--- backend/src/main/java/com/sunline/sunline_backend/service/MenuItemService.java
+++ backend/src/main/java/com/sunline/sunline_backend/service/MenuItemService.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@SuppressWarnings("null")
 public class MenuItemService {
 
     private final MenuItemRepository menuItemRepository;
